84dde626ee3f45d75120ccbb05561b8b8f63625c
Kristi <Kristi@Kristis-MacBook-Pro.local>
2017-05-07 13:28:38 -0700

    hangman progress

--- Hangman.py
+++ Hangman.py
@@ -1,41 +1,65 @@
 import random 
 # 1. Ask player if they want to play hangman?
-# - ask for user input "Do you want to play Hangman with me?" 
-# - player can select y or n
-# - if y, play hangman
-# - if n, print "maybe we can play later" then return question
 
 def asking_to_play_game():
     """asks player if they want to play"""
     prompt = "\nDo you want to play Hangman with me, yes or no? \n>>>"
     yes_or_no = raw_input(prompt)
     if yes_or_no == "yes":
-        print "Guess a letter!"
-        correct_word = pick_a_word()
-        print "Your word is", str(len(correct_word)), "characters long"
+        print "Great!"
     else:
-        print "Maybe we can play later!"
+        print "Maybe we can play later!" 
+        exit()
 
-# 2. Choose secret word 
+# 2. random selection of word 
 def pick_a_word():
-    """asks player if they want to pick a word""" 
-    my_list = ['learning', 'growing', 'changing', 'living']
+    """given a list of words, a word is selected""" 
+    my_list = ['python', 'coding', 'change', 'living']
     word = random.choice(my_list)
-    print word #keeping this in for trouble-shooting will remove for actual game
+    print word # print word # print word #keeping this in for trouble-shooting will remove for actual game
     return word
 
+# 3. Display blank lines for amount of letters in word that was selected 
+def display_len_lines_in_word(word_to_guess):
+    """display the blank lines in words set equal to length of word"""
+    
+    for length in word_to_guess:
+        print "_",
+    print "\n"      
+
+# 4. If player guesses a correct letter, replace blank letter with line 
+def pick_a_letter():
+    """given all the letters in the alphabet, the player is asked to pick any letter"""
+    prompt = "\nPick any letter in the alphabet, a-z. \n>>>"
+    letter = raw_input(prompt) 
+    index = word.find(letter)
+    
+    if index == -1:
+        print "That letter is not in the word"
+    else:
+       word_progress[index] = letter       
+    
+def display_incorrect_letters_guessed():
+    """if letter is incorrect, display letter"""
+    pass     
+
+ 
+def guessing_correct_word():
+    """if player guesses correct word before dashes are filled, they win!"""
+    pass 
+
 asking_to_play_game()
+word = pick_a_word()
+word_progress = list(len(word) * "_")
 
-# 2. Display blank lines (in string?) for amount of letters in word that was selected 
-# - 
-# 3. If player guesses a correct letter, replace blank letter with line 
-# -if letter is correct, put in place of line 
-# 4. If player guesses wrong, make a tally mark or deduct from number of guesses
-# -if letter not correct print letter below so player can see that they have already guesses that letter
-# 5. Player wins if they guess the word
-# -print "you win!"
-# 6. Player loses if they run out of guesses
-# -print "you lose!"
+print "Your word is", str(len(word)), "characters long"
+print word
 
+display_len_lines_in_word(word)
+
+while True:
+    pick_a_letter() 
+    print word_progress 
+        
 
 
